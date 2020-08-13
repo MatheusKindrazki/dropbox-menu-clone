1a9446352983725586904587e86c75ff6c9aae8b
Matheus <matheuskindrazki@gmail.com>
2020-08-13 10:50:52 -0300

    Updates

---
+++ precache-manifest.66f39bba89bf9ba45e49eeacaf287a7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8975df7122b5a8ab5dc524fbdab1d46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8d03e35fbed8d20bbf3",
+    "url": "/static/js/2.9983cd8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9983cd8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fd784ccb9e33f66d1b",
+    "url": "/static/js/main.f6f21590.chunk.js"
+  },
+  {
+    "revision": "3157dd1d1fc04b8b5bd4",
+    "url": "/static/js/runtime-main.feb6d2a8.js"
+  }
+]);
